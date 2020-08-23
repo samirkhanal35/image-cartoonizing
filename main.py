e4e2b086845a7b374c24d19ca5be00bfde0c44ae
samirkhanal35 <samirkhanal35@gmail.com>
2020-08-23 23:02:30 +0545

    special cartoon made

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 import cv2
 from PIL import Image, ImageTk
+import numpy as np
 
 #from tkinter import *
 window = tk.Tk()
@@ -104,13 +105,73 @@ def resize_img(img):
 
 def Cartoonizing():
     img_rgb = variables.img
-    img_oil = cv2.xphoto.oilPainting(img_rgb, 7, 1)
-    img_pencil, img1 = cv2.pencilSketch(img_rgb, sigma_s=60, sigma_r=0.07, shade_factor=0.05) 
+    # img_oil = cv2.xphoto.oilPainting(img_rgb, 3, 1)
+    numDownSamples = 2 # number of downscaling steps
+    numBilateralFilters = 5 # number of bilateral filtering steps
+
+    # -- STEP 1 --
+    # downsample image using Gaussian pyramid
+    img_color = img_rgb
+    for _ in range(numDownSamples):
+        img_color = cv2.pyrDown(img_color)
+
+    # repeatedly apply small bilateral filter instead of applying
+    # one large filter
     
-    img_pencil = cv2.adaptiveThreshold(img_pencil,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
-    img_pencil = cv2.cvtColor(img_pencil, cv2.COLOR_GRAY2BGR )
+    for _ in range(numBilateralFilters):
+        img_color = cv2.bilateralFilter(img_color, 10, 15, 15)
+
+    # upsample image to original size
     
-    img = cv2.bitwise_and(img_pencil, img_oil)
+    for _ in range(numDownSamples):
+        img_color = cv2.pyrUp(img_color)
+
+    
+    
+    """# img_pencil, img1 = cv2.pencilSketch(img_rgb, sigma_s=60, sigma_r=0.05, shade_factor=0.05) 
+    
+    kernel = np.ones((3,3), np.uint8) 
+    
+    # img_pencil = cv2.medianBlur(img_pencil, 5)
+    # img_pencil = cv2.adaptiveThreshold(img_pencil,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+    # img_pencil = cv2.dilate(img_pencil, kernel)  
+    
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY )
+    # img_gray = cv2.GaussianBlur(img_gray, (21, 21), 0)
+
+
+    image_blur = cv2.Canny(img_gray,100,200)
+    # image_blur = cv2.dilate(image_blur, kernel)  
+    image_blur = 255-image_blur
+    image_blur = cv2.GaussianBlur(image_blur, (21, 21), 0)
+    image_blur = cv2.erode(image_blur, kernel)
+    img_pencil = cv2.cvtColor(image_blur, cv2.COLOR_GRAY2BGR )
+
+    
+    
+    # img_pencil = cv2.bitwise_not(img_pencil)
+    
+    img_pencil = cv2.bitwise_or(img_pencil, img_color)
+
+    for _ in range(numBilateralFilters):
+        img_pencil = cv2.bilateralFilter(img_pencil, 10, 15, 15)
+
+    # img_pencil = cv2.cvtColor(img_pencil, cv2.COLOR_BGR2GRAY )
+    # img_pencil = cv2.adaptiveThreshold(img_pencil,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+
+    # img_pencil = cv2.cvtColor(img_pencil, cv2.COLOR_GRAY2BGR )
+
+    # img = img_pencil
+
+    img = cv2.bitwise_and(img_pencil, img_color)
+     """
+    img_gray = cv2.GaussianBlur(img_color, (21, 21), 0)
+    img = cv2.addWeighted(img_color, 1.5, img_gray, -0.9, 0)
+    
+   #contrast_stretching
+   #histogram_equalization
+
+    
 
     #resizing for image display
     variables.out_img = resize_img(img)
